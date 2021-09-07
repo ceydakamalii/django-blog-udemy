75b208da54f3e471d66c52672f6bc48faddf57be
Ceyda Kamali <ceydakamali3@gmail.com>
2021-09-07 15:57:59 +0300

    search

--- blog/views/home.py
+++ blog/views/home.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from blog.models import ArticleModel
 from django.core.paginator import Paginator
+from django.db.models import Q
+
 def home(request):
+    search = request.GET.get('search')
     articles = ArticleModel.objects.order_by("-id")
+    if search:
+        articles = articles.filter(
+            Q(title__icontains=search) |
+            Q(content__icontains=search)
+        ).distinct()
     page = request.GET.get('page')
     paginator = Paginator(articles, 2)
     context={
