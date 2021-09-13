8c492cc6f3145d695911bee627a6fcb6f6ead4d6
Ceyda Kamali <ceydakamali3@gmail.com>
2021-09-13 19:49:56 +0300

    profile update

---
+++ account/views/edit_profile.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from account.forms import EditProfileForms
+@login_required(login_url='/')
+def editProfile(request):
+    if request.method=='POST':
+       form = EditProfileForms(request.POST, request.FILES, instance=request.user)
+       if form.is_valid():
+           form.save()
+           messages.success(request, 'Profile successfully updated')
+
+
+    else:
+        form = EditProfileForms(instance=request.user)
+    return render(request, 'pages/edit_profile.html',context={'form':form})
