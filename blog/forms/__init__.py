b0c06bfb43d4b8a9671b1d3fbdf891e71c90e71a
Ceyda Kamali <ceydakamali3@gmail.com>
2021-09-13 16:06:03 +0300

    add comment

--- blog/forms/__init__.py
+++ blog/forms/__init__.py
@@ -1,3 +1,4 @@
 from .contact_form import ContactForm
 from .article_form import AddArticleForm
 from .updateArticle_form import UpdateArticleForm
+from .addComment import AddCommentForm
--- blog/views/detail.py
+++ blog/views/detail.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import ArticleModel
+from blog.forms import AddCommentForm
 
 def detail(request, slug):
+
     post = get_object_or_404(ArticleModel, slug=slug)
     comments = post.comments.all()
+    if request.method == 'POST' :
+        add_comment_form = AddCommentForm(data=request.POST)
+        if add_comment_form.is_valid():
+            comment= add_comment_form.save(commit=False)
+            comment.author= request.user
+            comment.article= post
+            comment.save()
+   
+    add_comment_form = AddCommentForm()
+
     return render(request, 'pages/detail.html',context={
         'post':post,
-        'comments':comments
+        'comments':comments,
+        'add_comment_form':add_comment_form
     })
