8e92e37af15449bfe318f3d94b96fa561fa30a40
Ceyda Kamali <ceydakamali3@gmail.com>
2021-09-13 19:12:28 +0300

    {% endif %}

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from account.views import logout
+from account.views import logout, changePassword
 
 urlpatterns = [
-    path('logout', logout, name='logout')
+    path('logout', logout, name='logout'),
+     path('change-password', changePassword, name='change-password'),
 ]
--- account/views/__init__.py
+++ account/views/__init__.py
@@ -1,1 +1,2 @@
 from .logout import logout
+from .change_password import changePassword
